5dec4060fcc365b6138c290ac49517ae113bb57b
Aslan Simaiev <72418039+aslansimaiev@users.noreply.github.com>
2025-12-30 15:30:48 +0200

    created xcode project

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by NIL on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
